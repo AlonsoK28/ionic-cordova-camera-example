e391edac61d590055deee084fe4ee409a9f325d3
C Alonso C Ortega <calonso011@yahoo.com.mx>
2021-05-26 12:32:05 -0500

    init project

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+// cordova
+import { Camera } from '@ionic-native/camera/ngx';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },  
+    Camera
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
